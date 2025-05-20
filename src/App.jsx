cd7f59c827b72530a6870fa41a915d974e5965ad
Jeet267 <ajtkr200@gmail.com>
2025-05-20 10:57:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Footer from './components/Footer'
+import HeroSection from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    <HeroSection/>
+    <Footer/>
+     
+       
+    </>
+  )
+}
+
+export default App
